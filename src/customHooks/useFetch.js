c8eaa3c6c671b26ac76a89a04a4f84bde6794ee4
BikashKakati <bikashkakati796@gmail.com>
2023-05-27 23:44:03 +0530

    Modify the Data Fetching Method

--- src/customHooks/useFetch.js
+++ src/customHooks/useFetch.js
@@ -1,39 +1,31 @@
-// const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US"
-
 
 import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
 
-const useFetch = (dataUrl) =>{
-   const[data, setData] = useState(null);
-   const [isLoading, setIsLoading] = useState(false);
-   const [error, setError] = useState(null);
+const useFetch = (dataUrl) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-   useEffect(() =>{
-     let subscribe = true;
-     const fetchData = async(url) =>{
-        setIsLoading(true);
-        try{
-          const {data} = await axios.get(url);
-          if(subscribe){
-            setData(data);
-            setError(null);
-          }
-        }catch(err){
-          if(subscribe){
-            
-            setError("Network Erro!");
-            setData([]);
-          }
-        }finally{
-          subscribe && setIsLoading(false);
-        }
-     }
-     fetchData(`https://api.themoviedb.org/3/movie/${dataUrl}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`);
+  useEffect(() => {
+    let subscribe = true;
+    setIsLoading(true);
 
-      return() =>{
-        subscribe = false;
-      }
-   },[dataUrl]);
-   return{data, isLoading, error};
+    fetchDataFromApi(dataUrl)
+      .then((res) => {
+        setIsLoading(false);
+        subscribe && setData(res);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError("Something went Wrong!");
+      })
+
+    return () => {
+      subscribe = false;
+    }
+  }, [dataUrl]);
+
+  return { data, isLoading, error };
 }
 export default useFetch;
