055970132d2439b9fe2dce485a0b053918a5e08d
BikashKakati <bikashkakati796@gmail.com>
2023-05-27 23:46:28 +0530

    Adding some new Pages
    Now we have Some new Pages
    -page not found page
    -search results page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React, {lazy, Suspense} from 'react'
 import './App.scss'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
+import PageNotFound from './pages/404/PageNotFound'
+import SearchResults from './pages/searchResults/SearchResults'
 
 const Home = lazy(() => import('./pages/home/Home'))
-const MovieList = lazy(() => import('./pages/movieList/MovieList'))
+const MovieExplore = lazy(() => import('./pages/moviesExplore/MoviesExplore'))
 const MovieDetails = lazy(() => import('./pages/movieDetails/MovieDetails'))
 
 const App = () => {
- 
+
 
   return (
     <>
@@ -18,8 +20,10 @@ const App = () => {
       <Routes>
 
         <Route path='/' element= {<Home/>} />
-        <Route path='/movies/:type' element= {<MovieList/>} />
-        <Route path='/movie/:id' element= {<MovieDetails/>} />
+        <Route path='/explore/:type' element= {<MovieExplore/>} />
+        <Route path='/details/:id' element= {<MovieDetails/>} />
+        <Route path='/search/:query' element={<SearchResults/>}/>
+        <Route path='*' element={<PageNotFound/>}/>
 
       </Routes>
       </Suspense>
---
+++ src/pages/searchResults/SearchResults.jsx
@@ -0,0 +1,9 @@
+import './SearchResults.scss';
+
+const SearchResults = () =>{
+    return(
+        <h2>Search results</h2>
+    )
+}
+
+export default SearchResults;
