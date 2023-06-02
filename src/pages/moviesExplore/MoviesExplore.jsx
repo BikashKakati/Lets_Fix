ea61fc1715e30999af128faa32bdc4dd87c23770
BikashKakati <bikashkakati796@gmail.com>
2023-06-02 23:56:07 +0530

    Modifying the movie explore page
    Now we have fully functional and responsive search bar

--- src/pages/moviesExplore/MoviesExplore.jsx
+++ src/pages/moviesExplore/MoviesExplore.jsx
@@ -1,44 +1,44 @@
 import React from 'react'
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-
-
 import Cards from '../../components/cards/Cards';
+import noResultsFound from '../../assets/no-results.png';
 import './MoviesExplore.scss'
 import useFetch from '../../customHooks/useFetch';
 const MovieList = () => {
 
-    const {render,inputData} = useSearchBox();
+
     const { type } = useParams();
-    const {data, isLoading, error} = useFetch(`/movie/${type ? type : popular}`);
+    const { data, isLoading, error } = useFetch(`/movie/${type ? type : popular}`);
+    const [inputData, setInputData] = useState("");
+
+    let getFilterData = data?.results?.filter(movie => {
+        return movie?.original_title?.toLowerCase().includes(inputData.toLowerCase())
+    })
 
 
-    // let getFilterData = data?.results?.filter(movie => {
-    //     return movie?.original_title?.toLowerCase().includes(inputData?.toLowerCase())
-    // })
-    
+
+
+
     return (
         <div className='movielist-container'>
             <div className="wrapper">
 
-            <div className="inner-container">
-                <div className="top-items">
-            <h4 className="movie-typeText">{(type ? type : "").toUpperCase()} </h4>
-             {render}
+                <div className="inner-container">
+                    <div className="top-items">
+                        <h4 className="movie-typeText">{(type ? type : "").toUpperCase()} </h4>
+                        <div className="search-box">
+                            <input type="search" placeholder='search...' className='text-area' value={inputData} onChange={(e) => setInputData(e.target.value)} />
+                        </div>
 
-                </div>
-                {
-
-                    data?.results ?
-
-                        (data?.results.map((movie, index) => (
+                    </div>
+                    {
+                        
+                        getFilterData?.length > 0 ?
+                        ( getFilterData?.map((movie) => (
                             isLoading ?
-                                // <SkeletonSt index={index}/>
-                                <div className="card-box" key={movie.id}>
+                                <div className="card-box" key={movie?.id}>
                                     <div className="card-imgBox skeleton"></div>
                                 </div>
 
@@ -46,33 +46,19 @@ const MovieList = () => {
                                 <Cards key={movie?.id} movieData={movie} />
                         )))
                         :
-                        <h4>No results found !</h4>
-                }
-            </div>
+                        (
+                            <div className="no-resultImg">
+                                <img src={noResultsFound} />
+                            </div>
+                        )
+
+                       
+
+                    }
+                </div>
             </div>
         </div>
     )
 }
-const useSearchBox = () =>{
-        const [inputData, setInputData] = useState("");
-    return{
-        inputData,
-        render:(
-            <div className="search-box">
-                <input type="search" placeholder='search...' className='text-area' value={inputData} onChange={(e) => setInputData(e.target.value)} />
-                <button>Search Here</button>
-            </div>
-    )}
-}
-// const SkeletonSt = ({index}) => {
-//     return (
-//         <SkeletonTheme key={index} baseColor='#202020' highlightColor='#444'>
-//             <div className="skeleton-card-box">
-//                 <Skeleton height={275} />
 
-//             </div>
-//         </SkeletonTheme>
-
-//     )
-// }
 export default MovieList
