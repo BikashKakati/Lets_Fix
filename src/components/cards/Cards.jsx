584181a4c1274c1fea5f8fa8f6d658a6bc00496d
BikashKakati <bikashkakati796@gmail.com>
2023-06-03 23:29:56 +0530

    Adding New react Library
    Now we have new react library
    -In cards we have circle Rating component using react circular progress bar
    - Also we have Fav icon

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,10 +1,12 @@
-
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import CircleRating from '../circleRating/CircleRating';
 import Img from '../lazyLoadImage/Img';
-import fallbackImg from '../../assets/no-poster.png'
-import './Cards.scss'
+import fallbackImg from '../../assets/no-poster.png';
+import './Cards.scss';
 
 const Cards = ({ movieData}) => {
+  const [favourite, setFavourite] = useState(false);
 
   const posterImgLink = movieData?.poster_path ? `https://image.tmdb.org/t/p/original${movieData.poster_path}` : fallbackImg;
   
@@ -13,15 +15,24 @@ const Cards = ({ movieData}) => {
     <div className="card-box">
 
       <div className="card-imgBox">
-        <Link to={`/movie/${movieData?.id}`}>
+        <Link to={`/details/${movieData?.id}`}>
 
           <Img src={posterImgLink} />
+          <CircleRating rating={movieData?.vote_average < 1 ? "1" : movieData?.vote_average?.toFixed(1)} />
 
         </Link>
+
       </div>
       <div className="card-aboutBox">
+
+        <div className="card-textBox">
         <h4 className="movie-title">{movieData ? movieData.original_title : ""}</h4>
         <span className="rel-date">{movieData ? movieData.release_date : ""}</span>
+
+        </div>
+        <div className={!favourite ? "card-favourite" : "card-favourite on"} onClick={()=>{setFavourite(!favourite)}}>
+        <i className="fa-solid fa-heart"></i>
+        </div>
       </div>
     </div>
 
