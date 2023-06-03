2fb68bd21d501ba671a93cb675a9377084df426e
BikashKakati <bikashkakati796@gmail.com>
2023-06-03 23:35:25 +0530

    Fixing some footer bug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -24,16 +24,16 @@ const Footer = () => {
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
-                    <i class="fa-brands fa-facebook-f"></i>
+                    <i className="fa-brands fa-facebook-f"></i>
                     </span>
                     <span className="icon">
-                    <i class="fa-brands fa-instagram"></i>
+                    <i className="fa-brands fa-instagram"></i>
                     </span>
                     <span className="icon">
-                    <i class="fa-brands fa-twitter"></i>
+                    <i className="fa-brands fa-twitter"></i>
                     </span>
                     <span className="icon">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
                     </span>
                 </div>
 
