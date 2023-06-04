5082077f92277c1cb7def3145365a1c0c0cc1396
BikashKakati <bikashkakati796@gmail.com>
2023-06-04 23:06:26 +0530

    Adding a new Video Section
    Now we have new video section with playing feature
    - using react video player
    -we can play thumbnails

---
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+import "./VideosSection.scss";
+
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
+import Img from "../../../components/lazyLoadImage/Img";
+
+const VideosSection = ({ data, loading }) => {
+    const [show, setShow] = useState(false);
+    const [videoId, setVideoId] = useState(null);
+
+    const loadingSkeleton = () => {
+        return (
+            <div className="skItem">
+                <div className="thumb skeleton"></div>
+                <div className="row skeleton"></div>
+                <div className="row2 skeleton"></div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="videosSection">
+            <div className="wrapper">
+                <div className="sectionHeading">Official Videos</div>
+                {!loading ? (
+                    <div className="videos">
+                        {data?.results?.map((video) => (
+                            <div
+                                key={video.id}
+                                className="videoItem"
+                                onClick={() => {
+                                    setVideoId(video.key);
+                                    setShow(true);
+                                }}
+                            >
+                                <div className="videoThumbnail">
+                                    <Img
+                                        src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`}
+                                    />
+                                    <i className="fa fa-play" aria-hidden="true"></i>
+                                </div>
+                                <div className="videoTitle">{video.name}</div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="videoSkeleton">
+                        {loadingSkeleton()}
+                        {loadingSkeleton()}
+                        {loadingSkeleton()}
+                        {loadingSkeleton()}
+                    </div>
+                )}
+            </div>
+            <VideoPopup
+                show={show}
+                setShow={setShow}
+                videoId={videoId}
+                setVideoId={setVideoId}
+            />
+        </div>
+    );
+};
+
+export default VideosSection;
