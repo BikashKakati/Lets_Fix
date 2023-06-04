e2ab9b4b30dcac00839f9e37a1ba1d7da81ade09
BikashKakati <bikashkakati796@gmail.com>
2023-06-04 23:12:21 +0530

    Video and Cast section added
    Now we have Video and Cast Section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,19 +4,21 @@ import "./Details.scss";
 
 import useFetch from "../../customHooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import VideosSection from "./videosSection/VideosSection";
 import Cast from "./cast/Cast";
 import Similar from "./carousels/Similar";
 import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
     const { id } = useParams();
-    const { data, loading } = useFetch(`/movie/${id}/videos`);
-    const { data: credits, loading: creditsLoading } = useFetch(`/movie/${id}/credits`);
+    const { data, isLoading } = useFetch(`/movie/${id}/videos`);
+    const { data: credits, isLoading: creditsLoading } = useFetch(`/movie/${id}/credits`);
 
     return (
         <div>
             <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
-            <Cast data={credits?.cast} loading={creditsLoading} />
+            <VideosSection data={data} loading={isLoading} />
+            <Cast data={credits?.cast} isLoading={creditsLoading} />
             <Similar id={id} />
             <Recommendation id={id} />
         </div>
