2ce3072e9b077b6c1620d8748a63007ae382812a
BikashKakati <bikashkakati796@gmail.com>
2023-06-07 23:45:34 +0530

    Adding wish list page
    Now we have wish list page

---
+++ src/pages/wishlist/WishList.jsx
@@ -0,0 +1,40 @@
+import { useSelector } from 'react-redux';
+import './WishList.scss';
+import Cards from '../../components/cards/Cards';
+import noResultsFound from '../../assets/no-results.png';
+const WishList = () =>{
+    const {wishList} = useSelector((state) => state.wishList);
+    
+    return(
+
+        <div className='wishlist-container'>
+            <div className="wrapper">
+                <div className="inner-container">
+                    <div className="top-items">
+                        <h4 className="movie-typeText">Wish List</h4>
+                    </div>
+                    {
+                        
+                        wishList.length===0 ?
+                        ( 
+                            <div className="no-resultImg">
+                                <img src={noResultsFound} />
+                            </div>
+                        )
+                        :
+                        (
+                            wishList?.map((movie) => (
+                                <Cards key={movie?.id} movieData={movie} />
+                            ))
+                            
+                        )
+
+                    }
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default WishList;
