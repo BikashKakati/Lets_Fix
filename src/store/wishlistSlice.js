2c21e78d193d856469b81094f4fa6d5ed23647a8
BikashKakati <bikashkakati796@gmail.com>
2023-06-12 23:49:08 +0530

    Local storage feature completed
    The local storage is properly work
    - Now the wish list movie cards are save in local storage

--- src/store/wishlistSlice.js
+++ src/store/wishlistSlice.js
@@ -1,24 +1,26 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+const cardsList = !!localStorage.getItem("favoriteCard") ? JSON.parse(localStorage.getItem("favoriteCard")) : []
+
 const wishlistSlice = createSlice({
     name: "wishList",
     initialState: {
-        wishList : [],
+        wishList : cardsList,
     },
     reducers : {
-        favoriteMovies : (state, action) =>{
-           state.wishList = action.payload;
-        },
+
         add : (state,action) => {
          state.wishList.push(action.payload);
+         localStorage.setItem("favoriteCard",JSON.stringify(state.wishList))
         },
         remove : (state,action) => {
             const {id} = action.payload;
             state.wishList = state.wishList.filter((item) => item.id !== id);
+            localStorage.setItem("favoriteCard",JSON.stringify(state.wishList))
         },
     },
     
 })
 
-export const {add,remove,favoriteMovies} = wishlistSlice.actions;
+export const {add,remove} = wishlistSlice.actions;
 export default wishlistSlice.reducer;
